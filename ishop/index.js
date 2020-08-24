d1d10e7955cbbbf1756da66b0f6517095124478e
Alexey Barkun <barkuns@gmail.com>
2020-08-24 12:48:43 +0300

    small fix

--- ishop/index.js
+++ ishop/index.js
@@ -3,31 +3,31 @@ var data = [
     {
         id: 321413,
         name: "Caffè Americano",
-        price: 285,
+        price: 2.85,
         image: "27de6f3e-2fa2-4f76-bb04-263ab13ad9aa",
         balance: 200
     }, {
         id: 213211,
         name: "Blonde Roast",
-        price: 245,
+        price: 2.45,
         image: "aedc77c9-e713-45b6-963a-2349ae4c77f7",
         balance: 100
     }, {
         id: 324444,
         name: "Caffè Misto",
-        price: 325,
+        price: 3.25,
         image: "66a1eebb-ef06-4e37-8906-74588d64aa18",
         balance: 200
     }, {
         id: 32422,
         name: "Featured Starbucks® Dark Roast Coffee",
-        price: 245,
+        price: 2.45,
         image: "300de9d9-0a7b-434e-b224-ac53cc94d54a",
         balance: 30
     }, {
         id: 325522,
         name: "Starbucks® Blonde Cappuccino",
-        price: 415,
+        price: 4.15,
         image: "9fbc7101-6a46-44c2-a25f-19a049389ba1",
         balance: 2
     },
@@ -58,7 +58,7 @@ var ItemsTable = React.createClass({
             if (item.balance <= 0) { return };
             var descriptionArr = [
                 React.DOM.li({ className: 'ItemName' }, item.name),
-                React.DOM.li({ className: 'ItemPrice' }, item.price)
+                React.DOM.li({ className: 'ItemPrice' }, item.price + ' $')
             ];
             var $description = React.DOM.ul({ className: "Description" }, ...descriptionArr);
             var $image = React.DOM.img({ className: 'ItemImage', src: getImagePath(item.image) });
