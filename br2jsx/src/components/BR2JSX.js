e33edc24e821f843e5c53e596bc092abc554a27e
Alexey Barkun <alexeyb@playtika.com>
2020-09-30 22:56:22 +0300

    fix keys warning

--- br2jsx/src/components/BR2JSX.js
+++ br2jsx/src/components/BR2JSX.js
@@ -3,11 +3,10 @@ import PropTypes from 'prop-types';
 
 class BR2JSX extends React.Component {
     render() {
-        let br = <br />
         let textArr = this.props.text.split(/\<br ?\/?>/)
         let result = []
         textArr.forEach((elem, i) => {
-            if (i) { result.push(br) }
+            if (i) { result.push(<br key={i}/>) }
             result.push(elem);
         });
         return (
